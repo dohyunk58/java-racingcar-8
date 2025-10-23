351e49484031ab58c0f91bfc4ed6a8fce6284b0d
dohyunk58 <hedge3x@gmail.com>
2025-10-24 06:13:57 +0900

    feat: 입력받은 자동차 이름으로 자동차 목록 생성 기능 추가
    - CarFactory에서 쉼표를 구분자로 하여 입력받은 문자열에서 이름을 분리함
    - 분리된 이름을 갖는 Car 객체의 List 생성 후 반환

---
+++ src/main/java/racingcar/Model/CarFactory.java
@@ -0,0 +1,18 @@
+package racingcar.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+    private static final String DELIMITER = ",";
+
+    public static List<Car> createCarList(String carNameInput) {
+        String[] names = carNameInput.split(DELIMITER);
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : names) {
+            carList.add(new Car(name.trim()));
+        }
+        return carList;
+    }
+}
---
+++ src/test/java/racingcar/Model/CarFactoryTest.java
@@ -0,0 +1,25 @@
+package racingcar.Model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarFactoryTest {
+
+    @DisplayName("쉼표로 구분된 문자열로 Car 리스트를 생성한다")
+    @Test
+    void createCarsFromNames() {
+        String carNameInput = "pobi,woni,jun";
+
+        List<Car> cars = CarFactory.createCarList(carNameInput);
+
+        assertThat(cars).hasSize(3);
+
+        assertThat(cars)
+                .extracting(Car::getName)
+                .containsExactly("pobi", "woni", "jun");
+    }
+}
